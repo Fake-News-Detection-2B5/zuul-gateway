541dd6019cf1e5a5eb2ee500e4b72136e241b379
adipeterca <adipeterca@gmail.com>
2021-05-11 13:46:15 +0300

    [UPDATE] Small changes

--- src/main/java/fakenewsdetection/zuulgateway/ZuulGatewayApplication.java
+++ src/main/java/fakenewsdetection/zuulgateway/ZuulGatewayApplication.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableZuulProxy
 @EnableEurekaClient
@@ -19,16 +21,30 @@ public class ZuulGatewayApplication {
 		SpringApplication.run(ZuulGatewayApplication.class, args);
 	}
 
+//	@Bean
+//	public CorsFilter corsFilter() {
+//		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		final CorsConfiguration config = new CorsConfiguration();
+//		config.setAllowCredentials(true);
+//		config.addAllowedOrigin("*");
+//		config.addAllowedHeader("*");
+//		config.addAllowedMethod("*");
+//		source.registerCorsConfiguration("/**", config);
+//		return new CorsFilter(source);
+//	}
+
 	@Bean
-	public CorsFilter corsFilter() {
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		final CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.addAllowedOrigin("*");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("*");
-		source.registerCorsConfiguration("/**", config);
-		return new CorsFilter(source);
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("*")
+						.allowedHeaders("*")
+						.allowedMethods("*")
+						.allowedOrigins("*");
+
+			}
+		};
 	}
 
 }
